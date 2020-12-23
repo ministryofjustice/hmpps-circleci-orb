a52eeff945637af2392066400c2cfaab16b2b8cb
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2020-12-23 15:20:58 +0000

    update, testing

--- src/scripts/deploy_env.sh
+++ src/scripts/deploy_env.sh
@@ -1,31 +1,30 @@
 #!/usr/bin/env bash
 
-if [[ ${CHART_VERSION} != "latest" ]]; then
-  CHART_VERSION_ARG="--version ${CHART_VERSION}"
-fi
-
 if [[ ${HELM_REPO} != "local" ]]; then
   helm repo add remote "${HELM_REPO}"
   CHART_NAME="remote/${CHART_NAME}"
 else
-  sed -i "s/appVersion: \".*\"/appVersion: \"${APP_VERSION}\"/g" "${CHART_NAME}/Chart.yaml"
+  sed -i "s/appVersion:.*/appVersion: \"${APP_VERSION}\"/g" "${CHART_NAME}/Chart.yaml"
 fi
 
 # Install/update any chart dependencies.
 helm dependency update "${CHART_NAME}"
 
-HELM_ARGS=("${CHART_VERSION_ARG}" \
-  "--wait" \
-  "--install" \
-  "--reset-values" \
-  "--timeout 5m" \
-  "--history-max 10" \
-  "--values values-${ENV_NAME}.yaml" \
-  "--set image.tag=${APP_VERSION}")
+HELM_ARGS=(--wait \
+  --install \
+  --reset-values \
+  --timeout 5m \
+  --history-max 10 \
+  --values "values-${ENV_NAME}.yaml" \
+  --set "image.tag=${APP_VERSION}")
 
 read -r -a extra_args <<< "${HELM_ADDITIONAL_ARGS}"
 
 HELM_ARGS+=("${extra_args[@]}")
+
+if [[ ${CHART_VERSION} != "latest" ]]; then
+  HELM_ARGS+=("--version ${CHART_VERSION}")
+fi
 
 if [[ "${RETRIEVE_SECRETS}" == 'aws' ]]; then
   # Load secrets from AWS secrets manager and have helm read values from stdin
