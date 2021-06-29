d5e1ffaf770f73fd8762978cec422d9465a56fc6
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2021-06-29 12:40:13 +0100

    [semver:patch] Fix changelog - Fail gracefully when no previous deployment found

--- src/scripts/version_history.sh
+++ src/scripts/version_history.sh
@@ -3,20 +3,32 @@
 # ensure consequences still work if this script blows up
 touch .deployment_changelog
 
-if [ "$K8S_DEPLOYMENT_NAME" == "PROJECT_NAME" ]; then
-  K8S_DEPLOYMENT_NAME="$CIRCLE_PROJECT_REPONAME"
+if [ "${K8S_DEPLOYMENT_NAME}" == "PROJECT_NAME" ]; then
+  K8S_DEPLOYMENT_NAME="${CIRCLE_PROJECT_REPONAME}"
 fi
+
+current_commit="$(echo "${APP_VERSION}" | cut -d'.' -f3)"
+
+# Try and get the currently deployed version
+K8S_PREVIOUS_APP_VERSION="$(kubectl get "deployment/${K8S_DEPLOYMENT_NAME}" --namespace="${KUBE_ENV_NAMESPACE}" -o=jsonpath='{.metadata.labels.app\.kubernetes\.io/version}' || true)"
 
 if [ "$K8S_PREVIOUS_APP_VERSION" == "" ]; then
-  K8S_PREVIOUS_APP_VERSION="$(kubectl get "deployment/$K8S_DEPLOYMENT_NAME" --namespace="$KUBE_ENV_NAMESPACE" -o=jsonpath='{.metadata.labels.app\.kubernetes\.io/version}')"
+  # if no previous version was found, set to current commit minus 1
+  echo "Previous deployment not found, showing current commit only." >> .deployment_changelog
+  previous_commit="${current_commit}^1"
+else
+  previous_commit="$(echo "${K8S_PREVIOUS_APP_VERSION}" | cut -d'.' -f3)"
 fi
 
-previous_commit="$(echo "$K8S_PREVIOUS_APP_VERSION" | cut -d'.' -f3)"
-current_commit="$(echo "$APP_VERSION" | cut -d'.' -f3)"
-
-PAGER="cat" git log --oneline --no-decorate \
-  --committer='noreply@github.com' --grep='#' \
-  "$previous_commit..$current_commit" \
-  > .deployment_changelog
+# Some apps may not have set the correct k8s label with a valid app version containing a sha1
+# Check $previous_commit sha1 is valid
+if git rev-parse --quiet --verify "${previous_commit}" &>/dev/null; then
+  PAGER="cat" git log --oneline --no-decorate \
+    --committer='noreply@github.com' --grep='#' \
+    "${previous_commit}..${current_commit}" \
+    >> .deployment_changelog
+else
+  echo "Changelog not available." > .deployment_changelog
+fi
 
 cat .deployment_changelog
