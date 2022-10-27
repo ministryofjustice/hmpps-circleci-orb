6456f564c48a82d291397bab5e41c77321b6915b
carlov20 <carlov20@users.noreply.github.com>
2022-10-27 16:10:11 +0100

    [semver:minor] added add configurable helm timeouts

--- src/scripts/deploy_env.sh
+++ src/scripts/deploy_env.sh
@@ -24,7 +24,7 @@ helm dependency update "${CHART_NAME}"
 HELM_ARGS=(--wait \
   --install \
   --reset-values \
-  --timeout 15m \
+  --timeout $HELM_TIMEOUT \
   --history-max 10 \
   --values "values-${ENV_NAME}.yaml")
 
