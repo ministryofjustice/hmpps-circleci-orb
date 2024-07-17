510f818660e9aab4211a5d5b47e437338e82f938
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2024-07-17 12:58:47 +0100

    fix

--- src/scripts/veracode_creds.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Randomly picking 1 of 5 api keys, to help avoid veracode API rate limits."
-X=$(( RANDOM % 4 ))
-export VERACODE_API_ID_X="VERACODE_API_ID_$X"
-export VERACODE_API_KEY_X="VERACODE_API_KEY_$X"
-echo "Using ${VERACODE_API_ID_X} from pool."
